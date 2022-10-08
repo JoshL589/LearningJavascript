bdfd5fb85df898c3c36e85f7a9e40c55a9b0c323
Joshua Li <joshuazli2020@gmail.com>
2022-10-08 17:14:45 +1000

    value vs reference testing

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-const createBooking = function (
-  flightNum,
-  numPassengers = 1,
-  price = 199 * numPassengers
-) {
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
 
-  console.log(booking);
-  bookings.push(booking);
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+
+// createBooking('LM123', undefined, 1000);
+
+const flight = 'LH234';
+const josh = {
+  name: 'Joshua Li',
+  passport: 2395823434,
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', 2);
-createBooking('LH123', 5);
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'Lh999';
+  passenger.name = 'Mr. ' + passenger.name;
 
-createBooking('LM123', undefined, 1000);
+  if (passenger.passport === 2395823434) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, josh);
+console.log(flight);
+console.log(josh);
